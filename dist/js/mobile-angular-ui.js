1bd84adf30c9afbadbd957d8a91bc65cca37709a
iamlalit <iamlalit.sachdeva@gmail.com>
2017-07-03 12:58:05 +0530

    updated for 2 new p[ages foe desktop and mobile in owc job apply

--- dist/js/mobile-angular-ui.js
+++ dist/js/mobile-angular-ui.js
@@ -3654,7 +3654,7 @@ angular.module('myApp', ['mobile-angular-ui.components']);
   ]);
 }());
 /*!
- * clipboard.js v1.7.1
+ * clipboard.js v1.6.0
  * https://zenorocha.github.io/clipboard.js
  *
  * Licensed MIT © Zeno Rocha
@@ -3665,7 +3665,7 @@ var DOCUMENT_NODE_TYPE = 9;
 /**
  * A polyfill for Element.matches()
  */
-if (typeof Element !== 'undefined' && !Element.prototype.matches) {
+if (Element && !Element.prototype.matches) {
     var proto = Element.prototype;
 
     proto.matches = proto.matchesSelector ||
@@ -3684,10 +3684,7 @@ if (typeof Element !== 'undefined' && !Element.prototype.matches) {
  */
 function closest (element, selector) {
     while (element && element.nodeType !== DOCUMENT_NODE_TYPE) {
-        if (typeof element.matches === 'function' &&
-            element.matches(selector)) {
-          return element;
-        }
+        if (element.matches(selector)) return element;
         element = element.parentNode;
     }
 }
@@ -4078,7 +4075,6 @@ module.exports = E;
                 var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
                 this.action = options.action;
-                this.container = options.container;
                 this.emitter = options.emitter;
                 this.target = options.target;
                 this.text = options.text;
@@ -4107,7 +4103,7 @@ module.exports = E;
                 this.fakeHandlerCallback = function () {
                     return _this.removeFake();
                 };
-                this.fakeHandler = this.container.addEventListener('click', this.fakeHandlerCallback) || true;
+                this.fakeHandler = document.body.addEventListener('click', this.fakeHandlerCallback) || true;
 
                 this.fakeElem = document.createElement('textarea');
                 // Prevent zooming on iOS
@@ -4126,7 +4122,7 @@ module.exports = E;
                 this.fakeElem.setAttribute('readonly', '');
                 this.fakeElem.value = this.text;
 
-                this.container.appendChild(this.fakeElem);
+                document.body.appendChild(this.fakeElem);
 
                 this.selectedText = (0, _select2.default)(this.fakeElem);
                 this.copyText();
@@ -4135,13 +4131,13 @@ module.exports = E;
             key: 'removeFake',
             value: function removeFake() {
                 if (this.fakeHandler) {
-                    this.container.removeEventListener('click', this.fakeHandlerCallback);
+                    document.body.removeEventListener('click', this.fakeHandlerCallback);
                     this.fakeHandler = null;
                     this.fakeHandlerCallback = null;
                 }
 
                 if (this.fakeElem) {
-                    this.container.removeChild(this.fakeElem);
+                    document.body.removeChild(this.fakeElem);
                     this.fakeElem = null;
                 }
             }
@@ -4177,8 +4173,8 @@ module.exports = E;
         }, {
             key: 'clearSelection',
             value: function clearSelection() {
-                if (this.trigger) {
-                    this.trigger.focus();
+                if (this.target) {
+                    this.target.blur();
                 }
 
                 window.getSelection().removeAllRanges();
@@ -4260,12 +4256,6 @@ module.exports = E;
         };
     }
 
-    var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
-        return typeof obj;
-    } : function (obj) {
-        return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-    };
-
     function _classCallCheck(instance, Constructor) {
         if (!(instance instanceof Constructor)) {
             throw new TypeError("Cannot call a class as a function");
@@ -4346,7 +4336,6 @@ module.exports = E;
                 this.action = typeof options.action === 'function' ? options.action : this.defaultAction;
                 this.target = typeof options.target === 'function' ? options.target : this.defaultTarget;
                 this.text = typeof options.text === 'function' ? options.text : this.defaultText;
-                this.container = _typeof(options.container) === 'object' ? options.container : document.body;
             }
         }, {
             key: 'listenClick',
@@ -4370,7 +4359,6 @@ module.exports = E;
                     action: this.action(trigger),
                     target: this.target(trigger),
                     text: this.text(trigger),
-                    container: this.container,
                     trigger: trigger,
                     emitter: this
                 });
